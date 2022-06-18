ff02433c222928eed971383fb6771b01242be8f9
Aisharya28071995 <gaishwarya58@gmail.com>
2022-06-18 19:17:17 +0530

    Commiting initial file

---
+++ assignment8_1/assignment8_1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment8_1
+{
+    internal class EmployeeWage
+    {
+        public static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation  Program on Master Branch");
+        }
+    }
+}
---
+++ assignment8_1/assignment8_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assignment8_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWage.DisplayMessage();
+        }
+    }
+}
